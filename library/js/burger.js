84d6a26b69c5b0fcac12ef094eec13593349d7e3
annaAzh <forarre@yandex.ru>
2023-07-14 17:00:43 +0300

    feat: add script to burger menu

---
+++ library/js/burger.js
@@ -0,0 +1,25 @@
+const burger = () => {
+  
+  const btn = document.querySelector('.burger');
+  const header = document.querySelector('.header-top');
+  const menuLink = document.querySelectorAll('.menu__link');
+
+  btn.addEventListener('click', () => {
+    header.classList.toggle('header-top--active');
+    document.body.classList.toggle('noscroll');
+    btn.classList.toggle('burger--active');
+  });
+
+  menuLink.forEach(elem => {
+    elem.addEventListener('click', closeMenu);
+  });
+
+  function closeMenu() {
+    header.classList.remove('header-top--active');
+    document.body.classList.remove('noscroll');
+    tn.classList.remove('burger--active');
+  }
+
+}
+
+export default burger;
