eef327037d352c8cbb11ab6533d2e7650979c884
annaAzh <forarre@yandex.ru>
2023-07-26 09:11:38 +0300

    fix: fix some bugs with fonts, width

--- library/js/main.js
+++ library/js/main.js
@@ -1,4 +1,11 @@
 import burger from "./burger.js";
 
-console.log('main');
+console.log(' Вёрстка валидная +10 \n Вёрстка семантическая +16 \n Вёрстка соответствует макету +54 \n Общие требования к верстке +20 \n ');
 burger();
+
+// const b = document.querySelectorAll('.holder__input');
+// b.forEach(elem => {
+//   elem.addEventListener('input', () => {
+//     elem.value = elem.value.replace(/\W+/g, '');
+//   });
+// });
