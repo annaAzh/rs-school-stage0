17089c732de3a995651f7d5748cb1646e2ad6b11
annaAzh <forarre@yandex.ru>
2023-08-08 13:18:27 +0300

    fix: add overlay for burger and fix adaptive property

--- library/js/burger.js
+++ library/js/burger.js
@@ -3,11 +3,20 @@ const burger = () => {
   const btn = document.querySelector('.burger');
   const header = document.querySelector('.header-top');
   const menuLink = document.querySelectorAll('.menu__link');
+  const overlay = document.querySelector('.overlay');
 
   btn.addEventListener('click', () => {
     header.classList.toggle('header-top--active');
     document.body.classList.toggle('noscroll');
     btn.classList.toggle('burger--active');
+    if (header.matches('.header-top--active')) {
+      overlay.classList.add('shown');
+      overlay.classList.remove('hide');
+    } else {
+      overlay.classList.remove('shown');
+      overlay.classList.add('hide');
+    }
+
   });
 
   menuLink.forEach(elem => {
@@ -19,6 +28,12 @@ const burger = () => {
     document.body.classList.remove('noscroll');
     btn.classList.remove('burger--active');
   }
+
+  overlay.addEventListener('click', () => {
+    closeMenu();
+    overlay.classList.remove('shown');
+    overlay.classList.add('hide');
+  });
 
 }
 
