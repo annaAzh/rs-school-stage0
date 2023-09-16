2e4b58b194bff245cfd7b1035d931e471c0dec65
annaAzh <forarre@yandex.ru>
2023-09-16 16:45:53 +0300

    feat: add logics for pressed play/paused/next/prev buttons

--- audio-player/main.js
+++ audio-player/main.js
@@ -22,4 +22,58 @@ const data = [
   },
 ];
 
+const btnPlay = document.querySelector('.play__img');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+const cover = document.querySelector('.player__img');
+const audio = document.querySelector('audio');
 
+let isPlay = false;
+let count = 0;
+
+audio.src = data[count].song;
+cover.src = data[count].img;
+
+btnPlay.addEventListener('click', () => {
+  if (!isPlay) {
+    playAudio();
+  } else {
+    pauseAudio();
+  }
+})
+
+function playAudio () {
+  audio.src = data[count].song;
+  cover.src = data[count].img;
+  audio.play();
+  isPlay = true;
+  btnPlay.src = 'assets/icons/pause.svg';
+}
+
+function pauseAudio () {
+  audio.pause();
+  isPlay = false;
+  btnPlay.src = 'assets/icons/play.svg';
+}
+
+nextBtn.addEventListener('click', playNext); 
+prevBtn.addEventListener('click', playPrev); 
+
+function playNext () {
+  
+  if (count < data.length - 1) {
+    count++;
+  } else {
+    count = 0;
+  }
+  playAudio();
+}
+
+function playPrev () {
+  if (count > 0) {
+    count--;
+  } else {
+    count = data.length - 1;
+  }
+  playAudio();
+}
