c5afd2e84f19924c67f097f16df7d7b62362b35f
annaAzh <forarre@yandex.ru>
2023-09-16 16:01:44 +0300

    feat: add media files

---
+++ audio-player/main.js
@@ -0,0 +1,25 @@
+const data = [
+  {
+    id: 0,
+    title: 'Michael e',
+    author: 'Matterhorn',
+    img: 'assets/cover/1.jpg',
+    song: 'assets/songs/01.mp3'
+  },
+  {
+    id: 1,
+    title: 'La Estacion Del Mar',
+    author: 'Punta Begona',
+    img: 'assets/cover/2.jpg',
+    song: 'assets/songs/02.mp3'
+  },
+  {
+    id: 2,
+    title: 'Velvet Dreamer',
+    author: 'Mystic Traveller',
+    img: 'assets/cover/3.jpg',
+    song: 'assets/songs/03.mp3'
+  },
+];
+
+
