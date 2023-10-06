516d00e2359d909f5ac2cfc9e4e922eaeaa4adaf
annaAzh <forarre@yandex.ru>
2023-10-06 14:57:54 +0300

    feat: reset game

--- random-game/main.js
+++ random-game/main.js
@@ -5,26 +5,38 @@ const score = document.querySelector('.score');
 let countScore = 0;
 score.textContent = countScore;
 
-
 const field = document.querySelector('.field');
 createPopup()
 showPopup('Start game');
 popup();
 
+function createCards() {
+  for (let i = 0; i < data.length; i++) {
+    const element = document.createElement('img');
+    element.classList.add('card');
+    element.src = './assets/image/blank.png';
+    element.setAttribute('alt', 'card');
+    field.append(element);
+  }
+}
+
+createCards()
+const cards = field.querySelectorAll('.card');
+
 let shuffledArray = [];
+
 function createShuffledArray() {
   shuffledArray = data.sort(() => Math.random() -0.5);
+  console.log(shuffledArray)
+  for (let i = 0; i < cards.length; i++) {
+    let card = cards[i];
+    card.setAttribute('data-id', i);
+    console.log(card)
+  }
 }
 createShuffledArray();
 
-for (let i = 0; i < shuffledArray.length; i++) {
-  const element = document.createElement('img');
-  element.classList.add('card');
-  element.src = './assets/image/blank.png';
-  element.setAttribute('data-id', i);
-  element.setAttribute('alt', 'card');
-  field.append(element);
-}
+
 function increaseAttempts() {
   countScore += 1;
   score.textContent = countScore;
@@ -36,7 +48,7 @@ let playArray = [];
 
 field.addEventListener('click', (e) => {
   if (e.target) {
-    const id = e.target.getAttribute('data-id');
+    let id = e.target.getAttribute('data-id');
     e.target.src = shuffledArray[id].src;
     if (openCard.length < 3) {
       openCard.push(id);
@@ -49,7 +61,6 @@ field.addEventListener('click', (e) => {
   }
 })
 
-const cards = field.querySelectorAll('.card');
 
 function checkTwoCards() {
   if (cardsName[0] === cardsName[1]) {
@@ -78,14 +89,22 @@ function checkWin() {
 }
 
 function resetGame() {
+  showPopup('You are win!');
   setTimeout(() => {
-    showPopup('You are win!');
-    countScore = 0;
-    score.textContent = countScore; 
-    createShuffledArray();
-    showPopup('Start game');
-    openCard = [];
-    cardsName = [];
-    playArray = [];
+    startNewGame()
   }, 1000);
+
+}
+
+function startNewGame() {
+  countScore = 0;
+  score.textContent = countScore;
+  openCard = [];
+  cardsName = [];
+  playArray = [];
+  createShuffledArray();
+  cards.forEach(card => {
+    card.classList.remove('disabled');
+    card.src = './assets/image/blank.png';
+  })
 }
