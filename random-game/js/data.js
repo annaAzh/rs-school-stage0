482a9b2b603e5af0a9c945c096c56e8be2994ab8
annaAzh <forarre@yandex.ru>
2023-10-06 09:07:52 +0300

    feat: add assets and data array

---
+++ random-game/js/data.js
@@ -0,0 +1,53 @@
+ const data = [
+  {
+    name: 'green',
+    src: '../assets/image/1.png'
+  },
+  {
+    name: 'orange',
+    src: '../assets/image/2.png'
+  },
+  {
+    name: 'yellow',
+    src: '../assets/image/3.png'
+  },
+  {
+    name: 'saturn',
+    src: '../assets/image/4.png'
+  },
+  {
+    name: 'red',
+    src: '../assets/image/5.png'
+  },
+  {
+    name: 'magnium',
+    src: '../assets/image/6.png'
+  },
+  {
+    name: 'green',
+    src: '../assets/image/1.png'
+  },
+  {
+    name: 'orange',
+    src: '../assets/image/2.png'
+  },
+  {
+    name: 'yellow',
+    src: '../assets/image/3.png'
+  },
+  {
+    name: 'saturn',
+    src: '../assets/image/4.png'
+  },
+  {
+    name: 'red',
+    src: '../assets/image/5.png'
+  },
+  {
+    name: 'magnium',
+    src: '../assets/image/6.png'
+  },
+
+];
+
+export default data;
